585a83f5e361328b1b8b0af8b0775ca4f758d349
Henriquesb1993 <henrique.onyx@hotmail.com>
2026-03-11 14:11:51 -0300

    Add PaginaHoras component to render hours module

---
+++ js/pagina-horas.js
@@ -0,0 +1,19 @@
+const PaginaHoras = {
+
+render(){
+
+document.getElementById("app").innerHTML = `
+
+<div class="card">
+
+<h2>Horas Realizadas</h2>
+
+<p>Módulo horas carregado</p>
+
+</div>
+
+`
+
+}
+
+}
