013743d1dc0bce345e98de507bf2c26ec4fafcfe
Henriquesb1993 <henrique.onyx@hotmail.com>
2026-03-11 14:10:12 -0300

    Implement basic Router functionality

--- js/router.js
+++ js/router.js
@@ -1,1 +1,26 @@
 
+const Router = {
+
+paginaAtual:null,
+
+init(){
+
+this.abrirPagina("horas")
+
+},
+
+abrirPagina(nome){
+
+this.paginaAtual = nome
+
+if(nome === "horas"){
+PaginaHoras.render()
+}
+
+if(nome === "viagens"){
+PaginaViagens.render()
+}
+
+}
+
+}
