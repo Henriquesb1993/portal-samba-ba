4dc0a1a3fbc9e8580ca8b953411158355626859f
Henriquesb1993 <henrique.onyx@hotmail.com>
2026-03-11 14:12:14 -0300

    Add PaginaViagens module with render method

---
+++ js/pagina-viagens.js
@@ -0,0 +1,21 @@
+conteúdo:
+
+const PaginaViagens = {
+
+render(){
+
+document.getElementById("app").innerHTML = `
+
+<div class="card">
+
+<h2>Viagens</h2>
+
+<p>Módulo viagens carregado</p>
+
+</div>
+
+`
+
+}
+
+}
